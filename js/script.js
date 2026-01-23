e19b18dc4d23d68cbd3172c0ef4a36ae88fc1402
Muad-syntax <mohammad.suaebummuad9smk.belajar.id>
2026-01-23 21:52:14 +0700

    update 0.1

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// 1. Tangkap elemen yang dibutuhkan
+// const hamburger = document.getElementById('hamburger');
+// const navMenu = document.getElementById('nav-menu');
+
+// 2. Tambahkan event listener saat tombol hamburger diklik
+// hamburger.addEventListener('click', () => {
+//     Toggle class 'active' pada hamburger (untuk animasi X)
+//     hamburger.classList.toggle('active');
+//     Toggle class 'active' pada menu (untuk memunculkan menu)
+//     navMenu.classList.toggle('active');
+// });
+
+// 3. (Opsional) Tutup menu saat salah satu link diklik
+// document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+//     hamburger.classList.remove('active');
+//     navMenu.classList.remove('active');
+// }));
